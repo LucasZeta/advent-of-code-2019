9c36df7d6f8fda0ad6d13bf5fa8d49f58ff7e42d
Lucas Souza <lucasdsdc@gmail.com>
2019-12-06 13:37:56 -0300

    Added day 3's first challenge

---
+++ src/main/kotlin/com/lucaszeta/adventofcode2019/day03/day03.kt
@@ -0,0 +1,50 @@
+package com.lucaszeta.adventofcode2019.day03
+
+import kotlin.math.abs
+
+fun List<String>.generatePath(): List<Pair<Int, Int>> {
+    val list = mutableListOf(0 to 0)
+
+    forEach { command ->
+        val direction = command.take(1)
+        val quantity = command.takeLastWhile { it.isDigit() }.toInt()
+
+        repeat(quantity) {
+            val lastPoint = list.last()
+
+            val newPoint = when (direction) {
+                "R" -> Pair(lastPoint.first + 1, lastPoint.second)
+                "L" -> Pair(lastPoint.first - 1, lastPoint.second)
+                "U" -> Pair(lastPoint.first, lastPoint.second + 1)
+                else -> Pair(lastPoint.first, lastPoint.second - 1)
+            }
+
+            list.add(newPoint)
+        }
+    }
+
+    return list.toList()
+}
+
+fun Set<Pair<Int, Int>>.getNearestManhattanDistance(): Int {
+    return filter { it != Pair(0, 0) }.map { it.getManhattanDistance() }.sorted().first()
+}
+
+fun Pair<Int, Int>.getManhattanDistance(): Int {
+    val origin = Pair(0, 0)
+
+    return abs(first - origin.first) + abs(second - origin.second)
+}
+
+fun main(args: Array<String>) {
+
+    val path1 = input.first().generatePath()
+    val path2 = input.last().generatePath()
+    
+    val intersections = path1.intersect(path2)
+
+    println(intersections)
+    
+    println(intersections.getNearestManhattanDistance())
+}
+
